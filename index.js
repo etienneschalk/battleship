676ac45752bd69933e9ab14b9d1fd4dce9e5867c
Etienne Schalk <etienne.schalk@grenoble-inp.org>
2020-03-24 21:04:06 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const CronJob = require('cron').CronJob;
+
+const cronjob = new CronJob('0 * * * * *', function() {
+  console.log("[Cron]")
+  console.log("\tusers connected:", io.engine.clientsCount);
+}, null, true, 'Europe/Paris');
+cronjob.start();
+
+var uniqueCount = 0;
+var availableRooms = [];
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log("user " + socket.id + " connected");
+  socket.uniqueCount = ++uniqueCount
+  io.emit('server message', "Welcome player " + uniqueCount + "! There are " + io.engine.clientsCount + " players online.")
+
+  if (availableRooms.length) {
+    console.log(availableRooms == true)
+    socket.join(availableRooms.pop());
+    console.log("An available room was joined")
+    // The game can be started
+  }
+  else {
+    // Use the uniqueCount of the socket as a room reference
+    socket.join(socket.uniqueCount)
+    availableRooms.push(socket.uniqueCount)
+    console.log(availableRooms)
+    console.log("No available room ! A new one has been created")
+    // Wait for a new player to join
+  }
+  console.log("rooms: " + JSON.stringify(io.sockets.adapter.rooms, undefined, 4))
+
+  socket.on('chat message', msg => {
+    console.log(socket.id + ' said: ' + msg);
+    io.emit('chat message', msg, socket.id, socket.uniqueCount);
+  })
+
+  socket.on('disconnect', () => {
+    console.log("user " + socket.id + " disconnected");
+  });
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
